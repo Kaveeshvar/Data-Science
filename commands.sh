2da547d3823dbe71897640e3aefb9419eb2bf7b9
Kaveeshvar <kaveskaves@gmail.com>
2026-01-21 12:15:06 +0530

    Day 1 files

---
+++ commands.sh
@@ -0,0 +1,18 @@
+git clone git@github.com:Kaveeshvar/Data-Science.git
+
+git status
+
+git add .
+
+#✅ Day 1 push
+git add python/day01.ipynb
+git commit -m "Add Day 1 Python notebook"
+git push origin main
+
+#✏️ Revise Day 1 later
+git add python/day01.ipynb
+git commit -m "Revise Day 1: added practice problems"
+git push origin main
+
+#Pull (when working from multiple machines)
+git pull origin main
